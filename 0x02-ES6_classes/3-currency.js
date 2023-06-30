9fc85cd12e1b2fc9fb5dc7429c8c9e3ad641899a
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 16:17:15 +0300

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+// Currency class
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Method to display full currency
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
