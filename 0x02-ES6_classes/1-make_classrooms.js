c224a5f50379707331aee9edb4f8bf92a08ccafb
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 19:50:15 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,14 +1,14 @@
 // Import the ClassRoom class
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 // initializeRooms function
-export default function initializeRooms() {
+function initializeRooms() {
   // Create an array of ClassRoom objects with different sizes
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
 
-  return rooms;
+  return [room1, room2, room3];
 }
+
+export default initializeRooms;
