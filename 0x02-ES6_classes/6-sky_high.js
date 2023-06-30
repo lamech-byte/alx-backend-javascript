3e65d9e9481d7cb730e7c6979c1a9493489d8a69
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 20:09:49 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,19 +1,19 @@
-// Import the Building class from 5-building.js
-import Building from './5-building.js';
+import Building from './5-building';
 
-// SkyHighBuilding class extends Building
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  // Getter for floors
+  get sqft() {
+    return this._sqft;
+  }
+
   get floors() {
     return this._floors;
   }
 
-  // Override evacuationWarningMessage method
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
