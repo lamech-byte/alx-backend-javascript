02754d96db9854a3e882d6db5c59c3a9d5c0a34d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 20:00:52 +0300

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+import Car from './10-car.js';
+
+const _range = Symbol('range');
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this[_range] = range;
+  }
+
+  get range() {
+    return this[_range];
+  }
+
+  cloneCar() {
+    const { brand, motor, color } = this;
+    return new Car(brand, motor, color);
+  }
+}
