3a0e589a4cc5248838bd910884f09eeb915a961a
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 20:07:30 +0300

    7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,16 +1,13 @@
-// Airport class
 export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  // toString method
   toString() {
     return `[object ${this._code}]`;
   }
 
-  // Default string description
   [Symbol.toStringTag]() {
     return this._code;
   }
