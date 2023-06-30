2a6251a9aacdd6bae049b6c8d4f9439d3d27e296
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 16:24:34 +0300

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+// Building class (considered abstract)
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method for evacuation warning message
+  evacuationWarningMessage() {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
