fee531113c59dbfb48ec27d475a8ee47c066ea96
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 16:07:17 +0300

    0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// ClassRoom class
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // Assign maxStudentsSize to _maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
