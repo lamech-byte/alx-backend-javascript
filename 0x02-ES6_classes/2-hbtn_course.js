c4c5bfe828264b385024099cbcb3a6bef6f7da1b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-02 21:45:06 +0300

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-// HolbertonCourse class
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._name = this.validateString(name, "Name");
-    this._length = this.validateNumber(length, "Length");
-    this._students = this.validateArray(students, "Students");
-  }
-
-  // Getter for name
-  get name() {
-    return this._name;
-  }
-
-  // Setter for name
-  set name(newName) {
-    this._name = this.validateString(newName, "Name");
-  }
-
-  // Getter for length
-  get length() {
-    return this._length;
-  }
-
-  // Setter for length
-  set length(newLength) {
-    this._length = this.validateNumber(newLength, "Length");
-  }
-
-  // Getter for students
-  get students() {
-    return this._students;
-  }
-
-  // Setter for students
-  set students(newStudents) {
-    this._students = this.validateArray(newStudents, "Students");
-  }
-
-  // Helper method to validate string attribute
-  validateString(value, attributeName) {
-    if (typeof value !== "string") {
-      throw new TypeError(`${attributeName} must be a string`);
-    }
-    return value;
-  }
-
-  // Helper method to validate number attribute
-  validateNumber(value, attributeName) {
-    if (typeof value !== "number") {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
-
-  // Helper method to validate array attribute
-  validateArray(value, attributeName) {
-    if (!Array.isArray(value)) {
-      throw new TypeError(`${attributeName} must be an array`);
-    }
-    return value;
-  }
-}
-=======
 // HolbertonCourse class
 export default class HolbertonCourse {
   constructor(name, length, students) {
@@ -124,4 +60,3 @@ export default class HolbertonCourse {
     return value;
   }
 }
->>>>>>> 8913d9ddab47f562e0bc38d8cbf43a334515d6a6
