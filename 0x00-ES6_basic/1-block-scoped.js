6da6cda8ae1ca92c5a7feda67fc46da3424cf1f4
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-02 20:25:47 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // Remove these lines: let task = true; and let task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
