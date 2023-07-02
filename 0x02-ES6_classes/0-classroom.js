b7ab22a7385911f7f31610a920a9abdd4a79a215
lamech-byte <gesachmoh@gmail.com>
2023-07-02 18:21:09 +0000

    ES6_classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,8 @@
 // ClassRoom class
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    // Assign maxStudentsSize to _maxStudentsSize
+    
+	  // Assign maxStudentsSize to _maxStudentsSize
     this._maxStudentsSize = maxStudentsSize;
   }
 }
