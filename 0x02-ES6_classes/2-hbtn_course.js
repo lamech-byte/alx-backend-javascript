8913d9ddab47f562e0bc38d8cbf43a334515d6a6
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-02 21:36:58 +0300

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,9 @@
 // HolbertonCourse class
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, "Name");
-    this._length = this.validateNumber(length, "Length");
-    this._students = this.validateArray(students, "Students");
+    this._name = this.validateString(name, 'Name');
+    this._length = this.validateNumber(length, 'Length');
+    this._students = this.validateArray(students, 'Students');
   }
 
   // Getter for name
@@ -13,7 +13,7 @@ export default class HolbertonCourse {
 
   // Setter for name
   set name(newName) {
-    this._name = this.validateString(newName, "Name");
+    this._name = this.validateString(newName, 'Name');
   }
 
   // Getter for length
@@ -23,7 +23,7 @@ export default class HolbertonCourse {
 
   // Setter for length
   set length(newLength) {
-    this._length = this.validateNumber(newLength, "Length");
+    this._length = this.validateNumber(newLength, 'Length');
   }
 
   // Getter for students
@@ -33,12 +33,12 @@ export default class HolbertonCourse {
 
   // Setter for students
   set students(newStudents) {
-    this._students = this.validateArray(newStudents, "Students");
+    this._students = this.validateArray(newStudents, 'Students');
   }
 
   // Helper method to validate string attribute
   validateString(value, attributeName) {
-    if (typeof value !== "string") {
+    if (typeof value !== 'string') {
       throw new TypeError(`${attributeName} must be a string`);
     }
     return value;
@@ -46,7 +46,7 @@ export default class HolbertonCourse {
 
   // Helper method to validate number attribute
   validateNumber(value, attributeName) {
-    if (typeof value !== "number") {
+    if (typeof value !== 'number') {
       throw new TypeError(`${attributeName} must be a number`);
     }
     return value;
