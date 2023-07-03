90b696183e4317e762974e83d3dfb98bb48b182c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-03 17:16:14 +0300

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userData = await signUpUser(firstName, lastName);
+    const fileData = await uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise]).then(results => {
-    return results.map(result => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  });
+    return [
+      { status: 'fulfilled', value: userData },
+      { status: 'fulfilled', value: fileData },
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error.message },
+      { status: 'rejected', value: error.message },
+    ];
+  }
 }
