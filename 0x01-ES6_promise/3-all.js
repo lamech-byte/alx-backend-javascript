316ab6831a0b565486c26f9015aa1b5fce0644fd
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-03 17:30:35 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photoResponse, userResponse] = results;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`body ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
 
-export default handleProfileSignup;
+handleProfileSignup();
