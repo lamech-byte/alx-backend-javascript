22f19025d87f26fd468c1f5a29d678fdb7a8b919
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-04 10:12:09 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   const values = Array.from(set);
+  if (startString === '') {
+    return values.join('-');
+  }
   const filteredValues = values.filter((value) => value.startsWith(startString));
   const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
   return cleanedValues.join('-');
