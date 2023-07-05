e6fa74a9fc2d710efc8af6fdc9de05ba2eb3c8df
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-05 17:43:49 +0300

    main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -9,29 +9,21 @@ interface Student {
 }
 
 // Create two students and store them in an array
-let student1: Student | undefined = undefined;
-let student2: Student | undefined = undefined;
-let studentsList: Student[] = [];
+const student1: Student = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 20,
+  location: 'New York',
+};
 
-if (!student1) {
-  student1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 20,
-    location: 'New York',
-  };
-}
+const student2: Student = {
+  firstName: 'Jane',
+  lastName: 'Smith',
+  age: 22,
+  location: 'London',
+};
 
-if (!student2) {
-  student2 = {
-    firstName: 'Jane',
-    lastName: 'Smith',
-    age: 22,
-    location: 'London',
-  };
-}
-
-studentsList = [student1, student2];
+const studentsList: Student[] = [student1, student2];
 
 // Render the table
 const table = document.createElement('table');
