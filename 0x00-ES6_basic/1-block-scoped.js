9ecc70978e3ccf31ecfa34753bad56894785f640
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-05 12:25:08 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -7,6 +7,9 @@ export default function taskBlock(trueOrFalse) {
     // Instead, you can use a different variable name inside the conditional block
     const task = true;
     const task2 = false;
+
+    task = task;
+    task2 = task2;
   }
 
   return [task, task2];
