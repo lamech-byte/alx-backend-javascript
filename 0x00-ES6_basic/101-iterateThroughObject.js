13fbd2d244213e67be0bd1d08ed295a23012060e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-05 14:11:39 +0300

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = [];
+  let employeeNames = [];
 
   for (const departmentEmployees of reportWithIterator) {
     employeeNames += departmentEmployees + ' | ';
