8109283008191644e6193e821ed0bea9783180ab
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-05 13:01:06 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line
+    // You can't reassign a new value to a const variable
     const task = true;
-    // eslint-disable-next-line
+    // Instead, you can use a different variable name inside the conditional block
     const task3 = false;
   }
 
