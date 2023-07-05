f8ff8f59f9bf12a817da1ec7d125886e8476661a
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-05 23:05:50 +0300

    main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,1 +1,21 @@
-i
+interface MajorCredits {
+  credits: number;
+  __brand: 'MajorCredits';
+}
+
+interface MinorCredits {
+  credits: number;
+  __brand: 'MinorCredits';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  const totalCredits = subject1.credits + subject2.credits;
+  return { credits: totalCredits, __brand: 'MajorCredits' };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  const totalCredits = subject1.credits + subject2.credits;
+  return { credits: totalCredits, __brand: 'MinorCredits' };
+}
+
+export { MajorCredits, MinorCredits, sumMajorCredits, sumMinorCredits };
