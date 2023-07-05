6462f504f17df087a87305032baf03e8160c21a4
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-05 12:26:56 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    // You can't reassign a new value to a const variable
-    // Instead, you can use a different variable name inside the conditional block
-    const task = true;
-    const task2 = false;
+    const taskInsideBlock = true;
+    const task2InsideBlock = false;
 
-    task = task;
-    task2 = task2;
+    task = taskInsideBlock;
+    task2 = task2InsideBlock;
   }
 
   return [task, task2];
