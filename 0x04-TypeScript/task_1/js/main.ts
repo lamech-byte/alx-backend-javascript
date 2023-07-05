b9cfa05e318b34dbbcc121656dfd57ddbd85522f
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-05 18:22:48 +0300

    main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,3 +21,56 @@ const teacher: Teacher = {
 };
 
 console.log(teacher);
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacherFunction: PrintTeacherFunction = printTeacher;
+
+console.log(printTeacherFunction("John", "Doe"));
+
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Director = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+interface Student {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass implements Student {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student: StudentClassInterface = StudentClass;
+
+const student1 = new student('John', 'Doe');
+console.log(student1.displayName()); // John
+console.log(student1.workOnHomework()); // Currently working
