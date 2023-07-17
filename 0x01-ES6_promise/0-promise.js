2201b9b2668ceaa4ba9e6ac692e491b572352368
Lamech M <gesachmoh@gmail.com>
2023-07-17 12:16:09 +0300

    ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,8 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform API request or any asynchronous operation here
-    // Example: Fetch data from an API
-    fetch('https://api.example.com')
-      .then((response) => {
-        // Handle the response
-        resolve(response);
-      })
-      .catch((error) => {
-        // Handle any error that occurred during the request
-        reject(error);
-      });
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const data = "Response from API";
+      resolve(data);
+    }, 2000);
   });
 }
-
