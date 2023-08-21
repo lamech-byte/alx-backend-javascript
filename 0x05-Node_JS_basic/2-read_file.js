ac2e75dbe90e35ce9645a350bf383006a27cb695
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-21 14:11:51 +0300

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1).map(line => line.split(','));
+
+    if (students.length === 0) {
+      throw new Error('Database is empty');
+    }
+
+    const fields = {};
+    students.forEach(student => {
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    const totalStudents = students.length;
+    console.log(`Number of students: ${totalStudents}`);
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+module.exports = countStudents;
