793fce457f932aa3a3e45f8be99b43c347c8d38c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-21 15:31:34 +0300

    6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -6,8 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!\n');
 });
 
-app.use((req, res) => {
-  res.status(404).send(`Cannot GET ${req.originalUrl}\n`);
+app.get('*', (req, res) => {
+  res.status(404).send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET ${req.url}</pre>
+</body>
+</html>`);
 });
 
 app.listen(1245, () => {
