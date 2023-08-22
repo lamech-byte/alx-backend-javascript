a43453a549763548ee8b731ee042bad24dc4df7d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-22 20:29:25 +0300

    1-stdin.test.js

--- 0x05-Node_JS_basic/1-stdin.test.js
+++ 0x05-Node_JS_basic/1-stdin.test.js
@@ -2,27 +2,29 @@ const { spawn } = require('child_process');
 
 describe('1-stdin.js', () => {
   it('should display name and handle exit', (done) => {
-    const process = spawn('node', ['1-stdin.js'], { stdio: 'pipe' });
-
+    const scriptPath = '1-stdin.js';
+    const input = 'John\n';
     const expectedOutput = [
-      'Welcome to Holberton School, what is your name?',
-      'Your name is: Alice\n',
-      'Your name is: Bob\n',
+      'Welcome to Holberton School, what is your name?\n',
+      'Your name is: John\n',
       'This important software is now closing\n'
     ];
 
+    const child = spawn('node', [scriptPath]);
     let currentIndex = 0;
 
-    process.stdout.on('data', (data) => {
+    child.stdin.write(input);
+
+    child.stdout.on('data', (data) => {
       expect(data.toString()).toBe(expectedOutput[currentIndex]);
       currentIndex++;
 
       if (currentIndex === expectedOutput.length) {
-        process.stdin.write('exit\n');
+        child.stdin.end();
       }
     });
 
-    process.on('exit', (code) => {
+    child.on('close', (code) => {
       expect(code).toBe(0);
       done();
     });
