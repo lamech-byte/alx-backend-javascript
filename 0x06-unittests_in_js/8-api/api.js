43030d30c4c42f267958449ebb5f257e16b31676
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-24 09:18:16 +0300

    api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,40 +1,12 @@
-const expect = require('chai').expect;
-const request = require('request');
-const { exec } = require('child_process');
+const express = require('express');
 
-describe('Index page', function () {
-  let serverProcess;
+const app = express();
+const port = 7865;
 
-  before(function (done) {
-    // Start the server as a separate process
-    serverProcess = exec('node api.js');
-    // Wait for a short time to ensure the server is up and running
-    setTimeout(() => done(), 1000);
-  });
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
 
-  after(function () {
-    // Kill the server process after the tests are done
-    serverProcess.kill();
-  });
-
-  it('should return correct status code and result', function (done) {
-    request('http://localhost:7865', function (error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('should log "API available on localhost port 7865"', function (done) {
-    const consoleLogStub = console.log;
-    console.log = function (message) {
-      expect(message).to.equal('API available on localhost port 7865');
-      done();
-    };
-    // Trigger the console log by making a request to the server
-    request('http://localhost:7865', function (error, response, body) {
-      // This request doesn't need any assertion, it's just to trigger the log
-    });
-    console.log = consoleLogStub; // Restore the original console.log
-  });
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
 });
