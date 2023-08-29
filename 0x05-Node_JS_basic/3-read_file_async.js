a9931c7775166ea3f71886b7db4917544080d17c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-29 08:16:09 +0300

    3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, fileContent) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = fileContent.trim().split('\n');
+      const students = {};
+
+      for (const line of lines) {
+        const fields = line.split(',');
+        if (fields.length === 4 && fields[0] !== 'firstname') {
+          const field = fields[3];
+          if (!students[field]) {
+            students[field] = [];
+          }
+          students[field].push(fields[0]);
+        }
+      }
+
+      console.log(`Number of students: ${lines.length - 1}`);
+      for (const field in students) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
