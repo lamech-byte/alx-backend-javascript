2b70359ac365ed9ff9867faf212dc6ec6e6335d8
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-30 08:55:58 +0300

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,21 +2,16 @@
 const fs = require('fs');
 
 const countStudents = (dataPath) => {
-  // Check if the file exists and is a regular file
   if (!fs.existsSync(dataPath) || !fs.statSync(dataPath).isFile()) {
     throw new Error('Cannot load the database');
   }
 
-  // Read file content and split into lines
   const content = fs.readFileSync(dataPath, 'utf-8').trim();
   const lines = content.split('\n');
-  
-  // Initialize studentGroups and fieldNames
   const studentGroups = {};
   const fieldNames = lines[0].split(',');
   const propNames = fieldNames.slice(0, fieldNames.length - 1);
 
-  // Process each line of data
   lines.slice(1).forEach((line) => {
     const record = line.split(',');
     const propValues = record.slice(0, record.length - 1);
@@ -27,12 +22,10 @@ const countStudents = (dataPath) => {
     studentGroups[field].push(Object.fromEntries(entries));
   });
 
-  // Calculate total number of students
   const totalStudents = Object.values(studentGroups)
     .reduce((total, group) => total + group.length, 0);
   console.log(`Number of students: ${totalStudents}`);
 
-  // Display the count of students in each field
   Object.entries(studentGroups).forEach(([field, group]) => {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
